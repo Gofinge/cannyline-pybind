291a312b258a629c3e6c24f193d4eec08317d152
Gofinge <gofinge.98@gmail.com>
2021-04-09 11:14:11 +0800

    remove build
    update

---
+++ cmake-build-debug/CMakeFiles/cannyline.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/xiaoyangwu/Documents/Projects/CProjects/cannyline-pybind/pybind11/CannyLine.cpp" "/Users/xiaoyangwu/Documents/Projects/CProjects/cannyline-pybind/cmake-build-debug/CMakeFiles/cannyline.dir/pybind11/CannyLine.cpp.o"
+  "/Users/xiaoyangwu/Documents/Projects/CProjects/cannyline-pybind/pybind11/np2mat/ndarray_converter.cpp" "/Users/xiaoyangwu/Documents/Projects/CProjects/cannyline-pybind/cmake-build-debug/CMakeFiles/cannyline.dir/pybind11/np2mat/ndarray_converter.cpp.o"
+  "/Users/xiaoyangwu/Documents/Projects/CProjects/cannyline-pybind/src/MetaLine.cpp" "/Users/xiaoyangwu/Documents/Projects/CProjects/cannyline-pybind/cmake-build-debug/CMakeFiles/cannyline.dir/src/MetaLine.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "CERES_FOUND"
+  "cannyline_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  "/usr/local/lib/python3.9/site-packages/numpy/core/include"
+  "/usr/local/Cellar/opencv/4.5.1_2/include/opencv4"
+  "/usr/local/Cellar/python@3.9/3.9.1_8/Frameworks/Python.framework/Versions/3.9/include/python3.9"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/cannyline.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cannyline.dir/pybind11/CannyLine.cpp.o"
+  "CMakeFiles/cannyline.dir/pybind11/np2mat/ndarray_converter.cpp.o"
+  "CMakeFiles/cannyline.dir/src/MetaLine.cpp.o"
+  "cannyline.cpython-39-darwin.so"
+  "cannyline.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cannyline.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
